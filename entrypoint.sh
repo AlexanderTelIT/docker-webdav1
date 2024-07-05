78b1ef55f35318c142955a9414c1834191044c01
AlexanderTelIT <99912583+AlexanderTelIT@users.noreply.github.com>
2024-07-05 10:55:00 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-HTPASSWD_FILE=/opt/webdav/.htpasswd
 
-if [ ! -f "$HTPASSWD_FILE" ]; then
-    echo "$WEBDAV_USER:$(openssl passwd -apr1 $WEBDAV_PASSWORD)" > $HTPASSWD_FILE
-fi
 
-sed -i "s/client_max_body_size 0;/client_max_body_size $WEBDAV_MAX_UPLOAD_SIZE;/g" /etc/nginx/http.d/default.conf
 
 exec "$@"
